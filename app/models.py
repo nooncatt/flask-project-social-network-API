df988495b6fa2d934b60d14f3620b93e82714fab
AA Karpenko <anastasha.me@gmail.com>
2024-10-02 18:41:29 +0300

    second commit

---
+++ app/models.py
@@ -0,0 +1,12 @@
+# models.py
+
+class User:
+    # todo: user class
+    def __init__(self, id, first_name, last_name, phone, email, score=0):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+        self.score = score
+
---
+++ app/views.py
@@ -0,0 +1,30 @@
+from . import app, USERS, models
+from flask import request, Response
+import json
+@app.route('/')
+def index():
+    return '<h1>hello world</h1>'
+
+@app.post('/user/create')
+def user_create():
+    data = request.get_json()
+    # data уже dict
+    id = len(USERS)
+    first_name = data['first_name']
+    last_name = data['last_name']
+    phone = data['phone']
+    email = data['email']
+
+    # todo: check thr phone and email for validity
+
+    user = models.User(id, first_name, last_name, phone, email)
+    USERS.append(user)
+    response = Response(
+        json.dumps({
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name
+            'phone': user.phone,
+            'email' : user.email
+        }), статус, хедеры, mimetipe = 'application/json')
+
