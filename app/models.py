cdedb3bb071a3496446e227df5c758a98b7873f5
AA Karpenko <anastasha.me@gmail.com>
2024-10-05 17:51:11 +0300

    Added some changes to create and get user

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 # models.py
+import re
+
 
 class User:
-    # todo: user class
     def __init__(self, id, first_name, last_name, email, total_reactions=0):
         self.id = id
         self.first_name = first_name
@@ -10,4 +11,13 @@ class User:
         self.total_reactions = total_reactions
         self.posts = []
 
+    @staticmethod
+    def is_valid_email(email):
+        if re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+            return True
+        else:
+            return False
+    # todo: Создание поста POST /posts/create
+    def create_post(self):
+        post_number = len(self.posts)
 
--- app/views.py
+++ app/views.py
@@ -1,21 +1,25 @@
 from . import app, USERS, models
 from flask import request, Response
 import json
+from http import HTTPStatus
+
+
 @app.route('/')
 def index():
     return '<h1>hello world</h1>'
 
-@app.post('/user/create')
+
+@app.post('/users/create')
 def user_create():
     data = request.get_json()
     # data уже dict
     id = len(USERS)
     first_name = data['first_name']
     last_name = data['last_name']
-    phone = data['phone']
     email = data['email']
 
-    # todo: check thr phone and email for validity
+    if not models.User.is_valid_email(email):
+        return Response(status=HTTPStatus.BAD_REQUEST)
 
     user = models.User(id, first_name, last_name, email)
     USERS.append(user)
@@ -24,7 +28,34 @@ def user_create():
             'id': user.id,
             'first_name': user.first_name,
             'last_name': user.last_name,
-            'email' : user.email
-        }), статус, хедеры, mimetipe = 'application/json')
+            'email': user.email,
+            'total_reactions': user.total_reactions,
+            'posts': user.posts,
+        }), HTTPStatus.OK, mimetype='application/json'
+    )
+    return response
+
+@app.get('/users/<int:user_id>')
+def get_user(user_id):
+    if user_id < 0 or user_id >= len(USERS):
+        return Response(status=HTTPStatus.BAD_REQUEST)
+    user = USERS[user_id]
+    response = Response(
+        json.dumps({
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'total_reactions': user.total_reactions,
+            'posts': user.posts,
+        }), HTTPStatus.OK, mimetype='application/json'
+    )
+    return response
 
 
+# todo: Создание поста POST /posts/create
+
+@app.post('/posts/create')
+def create_post():
+    data = request.get_json()
+    id = len(USERS)
